1d8f828360a0984a3e13498ea01a1d5518e65267
Kamil Czop <myknakryu@hotmail.com>
2021-03-14 02:07:33 +0100

    Revert "Revert "Rozwój klasy""
    This reverts commit 2012ca48e583b4ce1ddc56779e6754493e5ba90d.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace KatalogSamochodowy
 {
     class Program
     {
+        private static void Main(String[] args)
+        {
+            Console.WriteLine("Test");
+            Pojazd samochod = new Pojazd("Ford", "Focus", 2004, 1999, 20000, typ_skrzyni.automatyczna);
+            samochod.Wypisz();
         }
     }
 }
